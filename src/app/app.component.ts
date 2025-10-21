f3b8b4f127ac4150b51b14a46032f07bb3b99c24
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2025-10-21 16:29:50 +0200

    📌 Task 6: install ngx-env/builder library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ng-projects-management';
+
+  private readonly appName = import.meta.env.NG_APP_NAME;
+
+  constructor() {
+    this.title = this.appName;
+    console.log(this.appName)
+  }
 }
